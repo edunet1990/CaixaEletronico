28ff7b4c87cda93625f9925320d06d576ed26b57
Eduardo Estevão <eduardo.estevao.ext@pdg.com.br>
2018-09-13 19:02:33 -0300

    Primeiro commit

---
+++ CaixaEletronico/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CaixaEletronico
+{
+    class Conta
+    {
+        public int IdConta { get; private set; }
+        public string Nome { get;   set; }
+        public double Deposito { get;set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int idConta, string nome, double deposito)
+        {
+            this.IdConta = idConta;
+            this.Nome = nome;
+            this.Saldo = deposito;
+        }
+
+        public Conta(int idConta, string nome)
+        {
+            this.IdConta = idConta;
+            this.Nome = nome;
+            Saldo = 0.0;
+        }
+
+        public void depositar(double quantia)
+        {
+            Saldo += quantia;    
+        }
+
+        public void sacar(double quantia)
+        {
+
+            Saldo = Saldo - quantia - 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + IdConta + "Titular: " + Nome + ", Saldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ CaixaEletronico/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace CaixaEletronico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta x;
+            double valorInicial = 0;
+            Console.WriteLine("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o nome do ditular da conta: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n): ");
+            char ch = char.Parse(Console.ReadLine());
+            if(ch == 's')
+            {
+                Console.WriteLine("Digite o valor de depósito Inicial");
+                valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x = new Conta(numero, nome, valorInicial);
+            }
+            else
+            {
+                x = new Conta(numero, nome); 
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Conta Criada");
+            Console.WriteLine(x);
+
+
+            Console.WriteLine();
+            Console.Write("Digite um valor de depósito");
+            double dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.depositar(dep);
+            Console.WriteLine(x);
+
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque");
+            double saq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.sacar(saq);
+            Console.WriteLine();
+            Console.WriteLine("Conta Atualizada");
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
